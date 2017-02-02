124932ae8983f6f58dc5aab279be37830e8ee2c3
Amil <amilsil@gmail.com>
2017-02-02 18:26:42 +1100

    Use Unity for IOC

---
+++ CarPark/Config/UnityConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace CarPark.Config
+{
+    public class UnityConfig
+    {
+        public static IUnityContainer RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IRateCalculator, RateCalculator>();
+
+            return container;
+        }
+    }
+}
--- CarPark/Program.cs
+++ CarPark/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CarPark.Config;
+using Microsoft.Practices.Unity;
+using System;
 
 namespace CarPark
 {
@@ -10,6 +8,34 @@ namespace CarPark
     {
         static void Main(string[] args)
         {
+            /**
+             * This input date format can be improved
+             * either by a GUI or a human friendly format.
+             * But sticking to this to keep this simple.
+             */
+
+            Console.Write("Enter the EntryTime(in format 2017-01-01T10:00:00):");
+            var entryTimeStr = Console.ReadLine();
+
+            Console.Write("Enter the ExitTime(in format 2017-01-01T10:00:00):");
+            var exitTimeStr = Console.ReadLine();
+
+            // Proceed only if the times entered are valid.
+            DateTime entryTime, exitTime;
+            if (!DateTime.TryParse(entryTimeStr, out entryTime)
+                || !DateTime.TryParse(exitTimeStr, out exitTime))
+            {
+                Console.WriteLine("Please enter the times in the given format.");
+                Console.ReadKey();
+                return;
+            }
+
+            var container = UnityConfig.RegisterComponents();
+            var rateCalculator = container.Resolve<IRateCalculator>();
+            var calculatedRate = rateCalculator.GetRate(entryTime, exitTime);
+
+            Console.WriteLine($"Your parking charge is ${calculatedRate.Rate} on a {calculatedRate.RateType} rate");
+            Console.ReadKey();
         }
     }
 }
